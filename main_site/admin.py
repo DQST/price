fe7c7e816d09347b30515153e8d87993de0a2ae5
Dmitriy Yushko <rafiek57@gmail.com>
2016-10-17 16:58:57 +0300

    Update "main_site/admin.py"

--- main_site/admin.py
+++ main_site/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Categories)
-admin.site.register(Products)
+admin.site.register(SCategories)
+admin.site.register(SProducts)
 admin.site.register(Document)
