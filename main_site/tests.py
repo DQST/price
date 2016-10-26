2d091c017aa31c74192afd302b47794ee70bebbb
Dmitriy Yushko <rafiek57@gmail.com>
2016-10-26 10:01:38 +0300

    Add new test in "main_site/tests.py".

--- main_site/tests.py
+++ main_site/tests.py
@@ -1,24 +1,16 @@
 from django.test import TestCase, Client
 from .models import *
-from .views import *
-
-
-class ViewsTest(TestCase):
-	def setUp(self):
-		self.client = Client(enforce_csrf_checks=True)
-
-	def test_upload_file(self):
-		response = self.client.post('/login/', {'login':'admin', 'password':'qwert123'}, follow=True)
-		self.assertEqual(response.status_code, 302)
-		with open('D:/МАмалыш РРЦ NOORDI от 05.07.2016.xls', 'rb') as fp:
-			response  = self.client.post('/price/upload/', {'q':2, 'file': fp, 'row_head_offset': 1, 'row_data_offset': 2})
-		self.assertEqual(response.status_code, 200)
 
 
 class ModelsTest(TestCase):
 	def setUp(self):
 		Documents.objects.create(docfile='test_file.xls')
+		Products.objects.create(articul=631, name='NOORDI Коляска 3 в 1 SUN SPORT (автокресло+короб+прогулка,кожаная ручка) Молочный, шт.')
 
 	def test_document(self):
 		docs = Documents.objects.all()
 		self.assertEqual(len(docs), 1)
+
+	def test_articul(self):
+		o =  Products.objects.filter(articul=631)
+		self.assertEqual(len(o), 1)
