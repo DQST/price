98f5e4441d63398a21f2b8f660f7000bdff003eb
Dmitriy Yushko <rafiek57@gmail.com>
2016-10-28 15:33:03 +0300

    Remove "passenger_wsgi.py" from project.

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-cwd = os.getcwd()
-sys.path.append(cwd)
-sys.path.append(cwd + '/price')
-
-os.environ['DJANGO_SETTINGS_MODULE'] = "price.settings"
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
