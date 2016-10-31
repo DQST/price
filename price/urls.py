304a2307fff42554481b77e5d11421c6d0576dfd
Dmitriy Yushko <rafiek57@gmail.com>
2016-10-31 10:04:00 +0300

    Add new url pattern in "price/urls.py".

--- price/urls.py
+++ price/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^login/',	LoginView.as_view()),
     url(r'^logout/', LogoutView.as_view()),
     url(r'^price/', include('main_site.urls'), name='main'),
+    url(r'^parse/', include('price_parse.urls'), name='parse'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
