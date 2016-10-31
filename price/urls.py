53c0f7744a55b836f80664f7b7c96bcef231cbb8
Dmitriy Yushko <rafiek57@gmail.com>
2016-10-31 10:11:14 +0300

    Update url pattern in "price/urls.py"

--- price/urls.py
+++ price/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^login/',	LoginView.as_view()),
     url(r'^logout/', LogoutView.as_view()),
     url(r'^price/', include('main_site.urls'), name='main'),
-    url(r'^parse/', include('price_parser.urls'), name='parse'),
+    url(r'^parser/', include('price_parser.urls'), name='parser'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
