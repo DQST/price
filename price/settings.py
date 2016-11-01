267fdfe2ac44c665f3a227faced7a80256e26673
Dmitriy Yushko <rafiek57@gmail.com>
2016-11-01 12:24:44 +0300

    Update 'STATICFILES_DIRS = ['

--- price/settings.py
+++ price/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# STATIC_ROOT = '/home/tolkaiby/public_html/price.tolkai.by/static/'
+STATIC_ROOT = '/home/tolkaiby/public_html/price.tolkai.by/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = '/home/tolkaiby/public_html/price.tolkai.by/media/'
